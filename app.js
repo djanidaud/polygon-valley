e8352428f41beb018379e7bb33e330b3b79f86f3
Djani Daud <djani_daud@icloud.com>
2020-09-08 22:36:21 +0300

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'hbs');
-
+ 
 
 const publicDirectoryPath = __dirname + "/public";
 app.use(express.static(publicDirectoryPath));
