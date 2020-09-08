d65f4364829865164576546a8adcae54ad64be61
Djani Daud <djani_daud@icloud.com>
2020-09-08 23:18:38 +0300

    Setup app for Heroku

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
+
 app.set('view engine', 'hbs');
- 
+const port = process.env.PORT || 3000;
 
 const publicDirectoryPath = __dirname + "/public";
 app.use(express.static(publicDirectoryPath));
@@ -10,4 +11,4 @@ app.use(express.static(publicDirectoryPath));
 app.get('', (req, resp) => { 
     resp.render('index');
 });
-app.listen(3000);  
+app.listen(port, () => console.log('Server is up on port ' + port));  
